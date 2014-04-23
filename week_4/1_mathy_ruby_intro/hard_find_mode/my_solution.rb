c56a78bb686b5838a9d2a5b2474226d8c418c7bd
Jeff Kynaston <kynaston.jeffrey@gmail.com>
2014-04-22 20:43:42 -0700

    finished pseudocode

--- week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
+++ week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
@@ -4,17 +4,29 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 1. Pseudocode
 
 # What is the input?
+# -The input is an array of unknown elements
 # What is the output? (i.e. What should the code return?)
+# The output is the mode of the array - the element that is repeated the most
+# This can be a string or a number
 # What are the steps needed to solve the problem?
+# 1) Define a method called mode that takes one argument, (array)
+# 3) Create a default message if there is no mode, store that in a variable
+#    called arrayMode that will be returned at the end of the method
+# 4) Create an initial count variable and set it equal to 1
+# 5) Create a counter variable
+# 2) loop through the array and use the enumerable detect function to 
+#    find the number of each element in the araray. store that number in a variable 
+#    called count
+# 3) If an item in the array has a higher count than the initial count, 
+#    update arrayMode to be the that element in the array. 
 
 
 # 2. Initial Solution
-
 
 
 
