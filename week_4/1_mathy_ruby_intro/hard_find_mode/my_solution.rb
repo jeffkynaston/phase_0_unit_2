52f4106326324e3c0123b4402005f0c01382f2d5
Jeff Kynaston <kynaston.jeffrey@gmail.com>
2014-04-22 21:04:06 -0700

    completed refactoring and reflections

--- week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
+++ week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
@@ -59,8 +59,70 @@ puts mode(array)
 puts "------------------Next Array------------------------"
 array = ["apple", "banana", "clementine", "banana", "cherry", "strawberry", "cherry"]
 puts mode(array)
+
+
 # 3. Refactored Solution
 
+def mode(array)
+	initialCount = 1
+	arrayMode = []
+	i = 0
+	while array[i] 
+	  count = array.count {|j| j == array[i]}
+	  if count > initialCount
+	  	initialCount = count
+	  	arrayMode = [array[i]]
+	  elsif count == initialCount && count != 1
+	  	arrayMode.push(array[i])
+	  end
+	  i += 1
+	end
+	arrayMode = arrayMode.uniq
+	return arrayMode
+end
+
+# Refactored to use each instead of a while loop with counters
+
+def mode(array)
+	initialCount = 1
+	arrayMode = []
+	array.each do |element|
+	  count = array.count {|j| j == element}
+	  if count > initialCount
+	  	initialCount = count
+	  	arrayMode = [element]
+	  elsif count == initialCount && count != 1
+	  	arrayMode.push(element)
+	  end
+	end
+	arrayMode = arrayMode.uniq
+	return arrayMode
+end
 
 
 # 4. Reflection 
+
+# This was a tough one for me. Took me over an hour, but I was determined and
+# I finally figured it out. Part of my problem was using the enumerable 
+# detect function - it didn't work like I thought it would, and I ended
+# up using count instead. 
+
+# My initial solution strayed a little from my pseudocode because I didn't 
+# think through what I would need to do with an array that had multiple modes. 
+# When i was on the pseudocode step, I found it was hard to get through the 
+# entire solution without starting to code the method - I needed to try things
+# out first before I kept strategizing about the algorithm. 
+
+# You might also notice in my initial solution that there are a lot of puts
+# statements all over the place. I have found that this is my most effective
+# way of troubleshooting the logic in algorithms - My first shot at coding
+# doesn't always do what I want it to, so it's really helpful to have some
+# output at every step of the way through. This time, I added sentences to 
+# that output to help me understand what the function was doing. 
+
+# For my refactored solution, I removed the puts and it changed the average
+# runtime from .01 seconds to .002 seconds. I am also getting comfortable now
+# with using the each function (instead of a while loop) to iterate over
+# the array, so I refactored a version like that too. 
+
+# In all - great exercise. It was definitely worth the time I put into it. 
