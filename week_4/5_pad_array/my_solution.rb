ce3144d319ee97ca2144f352d4fdca4d884d9da0
Jeff Kynaston <kynaston.jeffrey@gmail.com>
2014-04-25 20:33:18 -0700

    finished pseudocode for first array method

--- week_4/5_pad_array/my_solution.rb
+++ week_4/5_pad_array/my_solution.rb
@@ -4,18 +4,28 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 1. Pseudocode
 
 # What is the input?
+# => The input is an array of any size or type, along with two arguments:
+#    a minimum length
+#    an optional pad value
 # What is the output? (i.e. What should the code return?)
+# => The output will be the intial array and, if needed, additoinal pad values
 # What are the steps needed to solve the problem?
-
+# 1) First, initialize the Array class so that we can make this a method of
+#    that class
+# 2) Then define a method called pad that takes two arguments, (min_length, pad_value = nil)
+#    this will make the second parameter optional, and pad the array with nil values 
+#    if no paramter is specificed
+# 3) Use an if statement to check if the length of the array is >= than min_length
+# 4) If yes,  return the original array
+# 5) else, push (min_length - self.length) pad_values to the array
+# 6) return the array
 
 # 2. Initial Solution
-
-
 
 
 # 3. Refactored Solution
