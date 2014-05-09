20d3141c5654e6ca983f41a2d1eb05dc3d950fd6
Jeff Kynaston <kynaston.jeffrey@gmail.com>
2014-05-09 14:57:27 -0700

    completed 5-refactoring, 8-bonus

--- week_6/5_refactoring/my_solution.rb
+++ week_6/5_refactoring/my_solution.rb
@@ -1,16 +1,66 @@
 # U2.W6: Refactoring for Code Readability
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 
-# Original Solution
+# Original Solution 
 
+class CreditCard
+
+    def initialize(number)
+        @number = number.to_s
+        raise ArgumentError.new("The Credit Card Number has to be 16 digits!!!") unless @number.length == 16
+    end
+    
+  def check_card
+    num_array = @number.split("")
+    num_array.each_with_index do |obj, i|
+        num_array[i] = (obj.to_i * 2) if i.even?
+     end
+     return true if num_array.join("").split("").map { |i| i.to_i }.reduce(:+) % 10 == 0
+     return false
+  end
+end
 
 
 
 # Refactored Solution
 
+class CreditCard
+
+  def initialize(number)
+      @number = number.to_s
+      @num_array = @number.split("")
+      @sum = 0
+      raise ArgumentError.new("The Credit Card Number has to be 16 digits!!!") unless @number.length == 16
+  end
+    
+  def check_card
+    self.double_digits
+    self.sum_digits
+    self.divisible_by_10?
+  end 
+    
+  # iterate over the array to double every even digit and replace that element with its double
+  def double_digits
+    @num_array.each_with_index do |obj, i|
+        @num_array[i] = (obj.to_i * 2) if i.even?
+     end
+   end
+
+  # join the array into a string, then re-split to separate double-digit numbers
+  # map the array of strings to integers and reduce (add) them into one number
+  def sum_digits
+    @sum = @num_array.join("").split("").map { |i| i.to_i }.reduce(:+)
+  end
+
+  def divisible_by_10?
+    return true if @sum % 10 == 0
+    return false
+  end
+
+end
 
 
 
@@ -18,9 +68,50 @@
 
 # DRIVER TESTS GO BELOW THIS LINE
 
+puts CreditCard.instance_method(:initialize).arity == 1
+puts CreditCard.instance_method(:check_card).arity == 0
+
+card = CreditCard.new(4563960122001999)
+puts card.instance_variable_get(:@number) == "4563960122001999"
+puts card.check_card == true
+
+card = CreditCard.new(4408041234567892)
+puts card.instance_variable_get(:@number) == "4408041234567892"
+puts card.check_card == false
+
+
+def assert
+  raise "Assertion Failed!" unless yield
+end
+
+assert { CreditCard.instance_method(:initialize).arity == 1 }
+assert { CreditCard.instance_method(:check_card).arity == 0 }
+
+card = CreditCard.new(4563960122001999)
+assert { card.instance_variable_get(:@number) == "4563960122001999"}
+assert { card.check_card == true }
+
+card = CreditCard.new(4408041234567892)
+assert { instance_variable_get(:@number) == "4408041234567892" }
+assert { card.check_card == false }
 
 
 
 
 
 # Reflection 
+
+#  My original solution was short and DRY, but not that clear. Obviously since I've 
+#  written and re-factored this already I know what's giong on, but the entire
+#  thing is only one method. A lot of the refactoring principals mentioned in this 
+#  challenge had to do with short, clear, readable methods so I wanted to refactor 
+#  my own solution to meet these standards. 
+
+#  made a method for each step of the algoritm, and then made a check_card method that
+#  called all three methods in a row. The names of my methods are discriptive, so you
+#  can tell what they are doing to the card without coments. For the code within
+#  the methods, I preferred to keep it as DRY as possible and leave a comment about
+#  make up for the complexity. 
+
+#  One thing I noticed ws that I had to create additional instance variable and define
+#  them in my initialize method. 
--- week_6/8_BONUS_CarClass/my_solution.rb
+++ week_6/8_BONUS_CarClass/my_solution.rb
@@ -1,30 +1,98 @@
 # U2.W6: Create a Car Class from User Stories
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [with: Andrew Koines Jr].
 
 
 # 2. Pseudocode
 
+# 1) Define the class Car
+# 2) Initialize Method - two arguments (model, color)
+#    Set instance variable @model = model
+#    Set instance variable @color = color
+#    Set instance variable @speed
+#    Set instance variable @odometer
+#    Set instance variable @direction
+
+#  3) Create Drive Method with Arguments (distance, speed)
+#     Puts last action at the end of the drive
+
+#  4) Create turn method (direction)
+
 
 
 # 3. Initial Solution
+
+class Car
+  def initialize(model,color)
+    @model=model
+    @color=color
+    @odometer=0
+    @direction=:north
+    @location=[0,0]
+  end
+  
+  def drive(distance,speed)
+    case @direction
+      when :north
+        @location[1]+=distance
+      when :south
+        @location[1]-=distance
+      when :east
+        @location[0]+=distance
+      when :west
+        @location[0]-=distance
+    end
+    @odometer += distance
+    puts "You drove #{distance} miles at #{speed} MPH. You've gone #{@odometer} miles."
+    puts "Your new location is #{@location} and you are now facing #{@direction}."
+  end
+  
+  def turn(direction)
+    @direction = direction.to_sym
+    puts "Your new direction is #{direction}."
+  end
+  
+end
 
 
 
 
 # 4. Refactored Solution
 
-
+# Our solution was already as dry as we could get it. In our drive method,
+# we factored out the puts statements and odometer updating from the case statments
+# so we didnt' have to repeat that code. 
 
 
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
+def assert
+  raise "Assertion Failed!" unless yield
+end
 
-
-
-
+car=Car.new("ford","green")
+assert { car.instance_variable_get(:@model) == "ford" }
+assert { car.instance_variable_get(:@color) == "green" }
+car.drive(0.25, 25)
+assert { car.instance_variable_get(:@location)[1] == 0.25 }
+car.turn(:east)
+assert{ car.instance_variable_get(:@direction) == :east }
+car.drive(1.5,35)
+assert{ car.instance_variable_get(:@location)[0] == 1.5 }
+car.drive(0.25,15)
+assert{ car.instance_variable_get(:@location)[0] == 1.75 }
+car.turn(:north)
+assert{ car.instance_variable_get(:@direction) == :north }
+car.drive(1.4,35)
+assert{ car.instance_variable_get(:@location)[1] == 1.65 }
 
 
 # 5. Reflection 
+
+#  Our method handled most of what the user stories were asking for. Adding a current speed
+#  for the car at all times would require a lot more work, as well as accellerate and decellerate
+#  methods that get called during the drive method. It would definitley be fun to code it out
+#  with more complexity, but unfortunately I don't have the time this week!
+# 
